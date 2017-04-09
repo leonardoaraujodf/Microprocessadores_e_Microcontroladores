3c77a09fef5b5f754f4b946b2af0892aeb0b722e
leonardoaraujodf <leonardoaraujodf@gmail.com>
2017-04-09 14:49:01 -0300

    Add files via upload

--- Respostas das Aulas/Aula_6_e_7_Codigos_assembly/Potenciacao/settings/potenciacao.Debug.cspy.ps1
+++ Respostas das Aulas/Aula_6_e_7_Codigos_assembly/Potenciacao/settings/potenciacao.Debug.cspy.ps1
@@ -23,9 +23,9 @@ param([String]$debugfile = "");
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\Leonardo\Documents\UnB\5 Semestre\Microprocessadores e Microcontroladores\Aula_6_e_7_Codigos\Potenciacao\settings\potenciacao.Debug.general.xcl" --backend -f "C:\Users\Leonardo\Documents\UnB\5 Semestre\Microprocessadores e Microcontroladores\Aula_6_e_7_Codigos\Potenciacao\settings\potenciacao.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\Leonardo\Documents\UnB\5 Semestre\Microprocessadores e Microcontroladores\Aula_6_e_7_Codigos_assembly\Potenciacao\settings\potenciacao.Debug.general.xcl" --backend -f "C:\Users\Leonardo\Documents\UnB\5 Semestre\Microprocessadores e Microcontroladores\Aula_6_e_7_Codigos_assembly\Potenciacao\settings\potenciacao.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\Leonardo\Documents\UnB\5 Semestre\Microprocessadores e Microcontroladores\Aula_6_e_7_Codigos\Potenciacao\settings\potenciacao.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\Leonardo\Documents\UnB\5 Semestre\Microprocessadores e Microcontroladores\Aula_6_e_7_Codigos\Potenciacao\settings\potenciacao.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\Leonardo\Documents\UnB\5 Semestre\Microprocessadores e Microcontroladores\Aula_6_e_7_Codigos_assembly\Potenciacao\settings\potenciacao.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\Leonardo\Documents\UnB\5 Semestre\Microprocessadores e Microcontroladores\Aula_6_e_7_Codigos_assembly\Potenciacao\settings\potenciacao.Debug.driver.xcl" 
 }
