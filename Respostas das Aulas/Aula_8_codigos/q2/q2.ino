dfeba8c61783c8a0ab7a9b6992afca64eb0fc7ff
Leonardo Amorim <leonardoaraujodf@gmail.com>
2017-04-28 22:57:17 -0300

    Novos Arquivos

---
+++ Respostas das Aulas/Aula_8_codigos/q2/q2.ino
@@ -0,0 +1,17 @@
+#include<msp430g2553.h>
+
+volatile unsigned int i;
+int main(void)
+{
+  WDTCTL = WDTPW | WDTHOLD;
+  P1OUT = 0;
+  P1DIR |= 0x41;
+  for(;;)
+  {
+    P1OUT ^= 0x41;
+    for(i=0;i<60000;i++);
+    P1OUT ^= 0x41;
+    for(i=0;i<30000;i++);   
+  }
+  
+}
---
+++ Respostas das Aulas/Aula_8_codigos/q3/q3.ino
@@ -0,0 +1,24 @@
+//Ligar LEDS enquanto o
+//botão estiver pressionado
+#include <msp430g2553.h>
+#define BTN BIT3
+#define LED1 BIT0
+#define LED2 BIT6
+int main(void){
+    WDTCTL = WDTPW | WDTHOLD;
+    P1OUT = 0;
+    P1REN |= BTN;
+    P1DIR |= LED1 + LED2;
+    P1OUT |= BTN; 
+    for(;;)
+      {
+      if((P1IN & BTN) == 0)
+      {
+        P1OUT |= LED1 + LED2;
+      }
+      else
+      {
+        P1OUT &= ~(LED1 + LED2);
+      }
+      }
+}
